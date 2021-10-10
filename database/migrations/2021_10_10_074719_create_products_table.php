838777286b8a49714c79dd94734dd2790c14d5a3
modip <mordip06@gmail.com>
2021-10-10 08:13:02 +0000

    products and categories models

--- database/migrations/2021_10_10_074719_create_products_table.php
+++ database/migrations/2021_10_10_074719_create_products_table.php
@@ -17,14 +17,20 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->string('short_description');
+            $table->string('short_description')->nullable();
             $table->text('description');
             $table->decimal('regular_price');
-            $table->decimal('sale_price');
-            $table->SKU('name');
+            $table->decimal('sale_price')->nullable();
+            $table->sting('SKU');
             $table->enum('stock_status',['instock', 'outofstock']);
             $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default('10');
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
         });
     }
 
